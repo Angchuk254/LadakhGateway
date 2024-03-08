1e21b5f0f0405e1877c6defdd668f51665b89b2e
sonam angchuk <sonamangchuk@mail.com>
2024-03-08 16:44:51 +0530

    added logged

--- connect.php
+++ connect.php
@@ -19,6 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   echo "Form submitted successfully!";
 } else {
   // Handle invalid request
+  alert('gggg')
+
   echo "Invalid request";
 }
 ?>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -52,6 +52,7 @@ function closeMenu() {
 function showPackges(packId) {
   const packData = packagesData[packId];
 
+
   if (packData) {
     const contentData = document.querySelector(".contentData");
     contentData.innerHTML = `
@@ -103,11 +104,11 @@ function showPackges(packId) {
           </div>
           <div class="row mt-5 mb-3">
           <div class="col-md-12 col-12">
-              <p class="popularPages">POPULAR HOTELS TO STAY</p>
+              <p class="popularPages">Recommendation To Explore</p>
           </div>
           <div class="col-md-3 col-sm-12 col-6 mt-3 ">
               <div class="card hotelView shadow ">
-                  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">
                         <div class="carousel-item active">
                           <img src="./assets/hideenValley/IMG-20240303-WA0028.jpg" class="d-block w-100" alt="...">
@@ -130,21 +131,21 @@ function showPackges(packId) {
           </div>
           <div class="col-md-3 col-sm-12 col-6 mt-3">
               <div class="card hotelView shadow ">
-                  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">
                         <div class="carousel-item active">
-                          <img src="../assets/hideenValley/ecoLodge.jpg" class="d-block w-100" alt="...">
+                          <img src="./assets/EcoLodge/IMG-20240306-WA0004.jpg" class="d-block w-100 " alt="..." height="222px">
                         </div>
                         <div class="carousel-item">
-                          <img src="./Backup/IMG-20240116-WA0002.jpg" class="d-block w-100" alt="...">
+                          <img src="./assets/EcoLodge/IMG-20240306-WA0007.jpg" class="d-block w-100" alt="..." height="222px">
                         </div>
                         <div class="carousel-item">
-                          <img src="./Backup/IMG-20240116-WA0002.jpg" class="d-block w-100" alt="...">
+                          <img src="./assets/EcoLodge/IMG-20240306-WA0003.jpg" class="d-block w-100" alt="..." height="222px">
                         </div>
                       </div>
                     </div>
                     <div class="card-body ">
-                      <p class="card-title stayNamer">Stakmo Eco Lodge Leh</p>
+                      <p class="card-title stayNamer"> Eco Lodge Leh, Stakmo</p>
                       <p class="card-text"><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i> <a href="https://www.google.com/travel/search?q=Stakmo%20eco%20lodge%20Leh&g2lb=2502548%2C2503771%2C2503781%2C2504511%2C4258168%2C4284970%2C4291517%2C4814050%2C4874190%2C4893075%2C4965990%2C10208620%2C10209060%2C72277293%2C72302247%2C72317059%2C72406588%2C72414906%2C72421566%2C72430562%2C72458066%2C72462234%2C72469155%2C72470440%2C72470899%2C72471280%2C72472051%2C72473738%2C72473841%2C72483525%2C72484083%2C72484736%2C72485656%2C72486593%2C72494250%2C72496942%2C72498532&hl=en-IN&gl=in&ssta=1&ts=CAEaKwopEicyJTB4MzhmZGVmZmVlNWYzZGQ2YjoweDVmNjdkZGEzOWY2NWIwNDU&qs=CAEyE0Nnb0l4ZUNXLTdtMDk3TmZFQUU4Ag&ap=ugEIb3ZlcnZpZXc&ictx=1" target="_blank" class="googleReview">Google review</a></p>
                       <a href="https://www.airbnb.co.in/rooms/32454697?source_impression_id=p3_1639556669_UCMRmcDz1R15SAhe" target="_blank" class="  packgeData">View on <i class="lni lni-airbnb" style="color: #FF385C;
                         ;"></i></a>
@@ -153,7 +154,7 @@ function showPackges(packId) {
           </div>
           <div class="col-md-3 col-sm-12 col-6 mt-3">
               <div class="card hotelView shadow ">
-                  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">
                         <div class="carousel-item active">
                           <img src="./Backup/IMG-20240116-WA0002.jpg" class="d-block w-100" alt="...">
@@ -176,7 +177,7 @@ function showPackges(packId) {
           </div>
           <div class="col-md-3 col-sm-12 col-6 mt-3">
               <div class="card hotelView shadow ">
-                  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">
                         <div class="carousel-item active">
                           <img src="./Backup/IMG-20240116-WA0002.jpg" class="d-block w-100" alt="...">
@@ -190,10 +191,9 @@ function showPackges(packId) {
                       </div>
                     </div>
                     <div class="card-body ">
-                      <p class="card-title stayNamer">Hidden Valley Homestay, Stakmo.</p>
-                      <p class="card-text"><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i><i class="lni lni-star-fill"></i> <a href="https://www.google.com/travel/hotels/hidden%20valley%20homestay/entity/CgoI1v_UuuCovtlPEAE?q=hidden%20valley%20homestay&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4306835%2C4317915%2C4371335%2C4401769%2C4419364%2C4429192%2C4515404%2C4524134%2C4545890%2C4549823%2C4596364%2C4617195%2C4619746%2C4621105%2C4270859%2C4284970&gsas=1&client=ms-android-xiaomi-rvo2b&v=12.10.7.23.arm64&hl=en-IN&gl=in&ssta=1&rp=ENb_1LrgqL7ZTxDW_9S64Ki-2U84AkAASAHAAQI&ictx=1&ved=0CAAQ5JsGahcKEwjYzs2szLCEAxUAAAAAHQAAAAAQAw&utm_campaign=sharing&utm_medium=whatsapp&utm_source=htls&ts=CAEaIAoCGgASGhIUCgcI6A8QAhgWEgcI6A8QAhgXGAEyAhAAKgQKABoA" target="_blank" class="googleReview">Google review</a></p>
-                      <a href="https://www.airbnb.co.in/rooms/32454697?source_impression_id=p3_1639556669_UCMRmcDz1R15SAhe" target="_blank" class="  packgeData">View on <i class="lni lni-airbnb" style="color: #FF385C;
-                        ;"></i></a>
+                      <p class="card-title stayNamer mb-3">One Day Free Trekking in Ladakh</p>
+                      
+                      <a  class="  packgeData" onclick="showPackges('pack5')" style="cursor:pointer;">View more</a>
                     </div>
                 </div>
           </div>
@@ -202,7 +202,26 @@ function showPackges(packId) {
           `;
     scrollToTop();
   }
+  if (packagesData.pack1===packData) {
+    document.querySelector('.pack1Trip').style.backgroundImage = 'url(./assets/Nubra/nuPang.jpg)';
+}
+if (packagesData.pack3===packData) {
+  document.querySelector('.pack1Trip').style.backgroundImage = 'url(./assets/Nubra/nubraScreen.jpg)';
+}
+if (packagesData.pack4===packData) {
+  document.querySelector('.pack1Trip').style.backgroundImage = 'url(./assets/prateek-katyal-8wmEXx5QtgM-unsplash.jpg)';
+}
+if (packagesData.pack6===packData) {
+  document.querySelector('.pack1Trip').style.backgroundImage = 'url(./assets/DSC_0035.jpg)';
+}
+if (packagesData.pack4===packData) {
+  document.querySelector('.pack1Trip').style.backgroundImage = 'url(./assets/prateek-katyal-8wmEXx5QtgM-unsplash.jpg)';
+}
+if (packagesData.pack2===packData) {
+  document.querySelector('.pack1Trip').style.backgroundImage = 'url(./assets/para.webp)';
+}
   showNestedData(packId, "overview");
+  
 }
 
 function showNestedData(packId, nestedKey) {
@@ -278,17 +297,19 @@ function submitForm() {
   function sendFormData() {
     var formData = new FormData(document.getElementById("myForm"));
     var xhr = new XMLHttpRequest();
-
+   
     xhr.open("POST", "connect.php", true);
 
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
         console.log(xhr.responseText);
+       
         // You can add additional logic here based on the server response
       }
     };
 
     xhr.send(formData);
+   
   }
 }
 
